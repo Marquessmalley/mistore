77b0470cf111d499212f2b24293e2ce997a44582
Marques Smalley <marquessmalley@gmail.com>
2023-06-13 14:16:26 -0400

    app connected to db

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 const app = require("./app");
+const dbConnect = require("./config/dbConnect");
 const port = process.env.PORT || 5000;
+
+dbConnect();
 
 app.listen(port, () => console.log(`Serve starting on port: ${port}`));
