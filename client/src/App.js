8445d9c6f3069b1fb946901adbc8b4be76ee72fe
Marques Smalley <marquessmalley@gmail.com>
2023-06-20 10:44:02 -0400

    setup react router foundation

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import AdminLayout from "./components/Layouts/Admins/AdminLayout";
+import AdminDash from "./pages/admin/AdminDash";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
+    <Routes>
+      {/* PUBLIC ROUTES */}
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+
+      {/* PRIVATE ROUTES */}
+      <Route path="admin-dash" element={<AdminLayout />}>
+        <Route index element={<AdminDash />} />
+      </Route>
+    </Routes>
   );
 }
 
