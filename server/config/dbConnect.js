cefa1e765d8169daaad8de0bb4c5e2d81bc65858
Marques Smalley <marquessmalley@gmail.com>
2023-08-16 15:01:37 -0400

    db uri string chnage

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const dbConnect = async () => {
   try {
     await mongoose
-      .connect(process.env.DATABASE)
+      .connect(
+        "mongodb+srv://marquessmalley:GvoNZegve8HnCwWy@cluster0.4ggpj9a.mongodb.net/mistrain-db"
+      )
       .then(() => console.log("Connected to database successfully"));
   } catch (err) {
     console.error(err);
