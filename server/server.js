faf08eca33ed58df2ba4ab1c3352126eb8003b9a
Marques Smalley <marquessmalley@gmail.com>
2023-08-17 13:13:29 -0400

    added console.log

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const app = require("./app");
 const dbConnect = require("./config/dbConnect");
 const port = process.env.PORT || 8000;
+console.log(process.env.PORT);
 
 dbConnect();
 
