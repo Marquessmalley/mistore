ae10ce0f91564d5f9e833c62bdefa397d88b981b
Marques Smalley <marquessmalley@gmail.com>
2023-08-18 12:41:58 -0400

    server chnage

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -62,8 +62,8 @@ module.exports.login = async (req, res, next) => {
 
     res.cookie("jwt", refreshToken, {
       httpOnly: true, // accessible only by web server
-      // secure: true, // https
-      sameSite: "Lax", // cross-site cookie
+      secure: false, // https
+      sameSite: "None", // cross-site cookie
       maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expir
     });
 
