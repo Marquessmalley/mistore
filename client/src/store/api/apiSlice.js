148a6f5a46bad77d46a0b0c16e34ecf86b8d42d0
Marques Smalley <marquessmalley@gmail.com>
2023-08-25 13:39:05 -0400

    updated rtk query base url

--- client/src/store/api/apiSlice.js
+++ client/src/store/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredential } from "../slices/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://name.onrender.com",
+  baseUrl: "https://mistrain-api.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
