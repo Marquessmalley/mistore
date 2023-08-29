3f1697c0a6d9b789f8670552d3a25a46b7cb92da
Marques Smalley <marquessmalley@gmail.com>
2023-08-29 15:46:34 -0400

    added path

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -64,7 +64,8 @@ module.exports.login = async (req, res, next) => {
       httpOnly: true, // accessible only by web server
       secure: true, // The Secure flag ensures that cookies are only transmitted over secure (HTTPS) connections
       sameSite: "None", // cross-site cookie
-      domain: "mistrain.onrender",
+      domain: "mistrain.onrender.com",
+      path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expir
     });
 
