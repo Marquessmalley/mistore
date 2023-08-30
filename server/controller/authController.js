b19dea1fb17a374cb78ddb212561489dcbfb3c95
Marques Smalley <marquessmalley@gmail.com>
2023-08-30 14:29:25 -0400

    removed properties

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -63,9 +63,9 @@ module.exports.login = async (req, res, next) => {
     res.cookie("jwt", refreshToken, {
       httpOnly: true, // accessible only by web server
       secure: true, // The Secure flag ensures that cookies are only transmitted over secure (HTTPS) connections
-      sameSite: "None", // cross-site cookie
+      // sameSite: "None", // cross-site cookie
       domain: "mistrain.onrender.com",
-      path: "/admin-dash",
+      // path: "/admin-dash",
       maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expir
     });
 
