bc3c95c092938605dfb9289afd11bcfe85d7af45
Marques Smalley <marquessmalley@gmail.com>
2023-08-30 12:45:13 -0400

    removed domain

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -64,7 +64,6 @@ module.exports.login = async (req, res, next) => {
       httpOnly: true, // accessible only by web server
       secure: true, // The Secure flag ensures that cookies are only transmitted over secure (HTTPS) connections
       sameSite: "None", // cross-site cookie
-      domain: "mistrain.onrender.com",
       maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expir
     });
 
