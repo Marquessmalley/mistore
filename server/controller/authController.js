891276664f0b01bec961ff8c284300e374833631
Marques Smalley <marquessmalley@gmail.com>
2023-09-08 23:20:14 -0400

    changed sameSite

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -65,7 +65,7 @@ module.exports.login = async (req, res, next) => {
     res.cookie("jwt", refreshToken, {
       httpOnly: true, // accessible only by web server
       secure: true, // The Secure flag ensures that cookies are only transmitted over secure (HTTPS) connections
-      sameSite: "None", // cross-site cookie
+      sameSite: "lax", // cross-site cookie
       maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expir
       domain: ".mistrain.onrender.com",
     });
