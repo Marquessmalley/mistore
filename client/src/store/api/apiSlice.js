d3ff338ca7d5b2da2f88de66cb63055562868430
Marques Smalley <marquessmalley@gmail.com>
2023-09-22 15:50:00 -0400

    changed domain url's

--- client/src/store/api/apiSlice.js
+++ client/src/store/api/apiSlice.js
@@ -2,8 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredential } from "../slices/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  // baseUrl: "https://mistrain-api.onrender.com",
-  baseUrl: "http://localhost:8000",
+  baseUrl: "https://mistrain-api.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
